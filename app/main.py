14d84383e0b50716e3872f280dbcef4a9d0d6a0c
Bashar <miniappape@gmail.com>
2025-07-17 20:21:35 +0400

    add app details

--- app/main.py
+++ app/main.py
@@ -4,7 +4,13 @@ from app.db.database import engine
 from app.api.todo import router as todos_router
 from app.models.todo import Base as TodosModel
 from app.api.auth import router as auth_router
-app = FastAPI()
+app = FastAPI(title="ToDo API",
+    description="API to manage ToDo tasks",
+    version="1.0.0",
+    contact={
+        "name": "Bashar",
+        "email": "bashar@example.com",
+    })
 TodosModel.metadata.create_all(engine)
 app.include_router(todos_router)
 app.include_router(auth_router)
